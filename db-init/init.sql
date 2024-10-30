4c060f4915097fff20bd31665f45474ee9932e6f
Eldhose-Peter <eldhosepeter2000@gmail.com>
2024-10-30 19:13:53 +0530

    fixed authentication

--- db-init/init.sql
+++ db-init/init.sql
@@ -6,11 +6,7 @@ CREATE TABLE IF NOT EXISTS users (
     id SERIAL PRIMARY KEY,
     username VARCHAR(80) NOT NULL,
     email VARCHAR(255) NOT NULL UNIQUE,
+    password TEXT NOT NULL,
     created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
 );
 
--- Optional: Insert sample data
-INSERT INTO users (username, email)
-VALUES
-    ('demo_user', 'demo_user@example.com'),
-    ('test_user', 'test_user@example.com');
